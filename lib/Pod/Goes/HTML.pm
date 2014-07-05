73e08322c0733a7d135ff89e7984ac6e0974bf6b
Mikhail Khorkov <m.s.khorkov@gmail.com>
2014-07-05 23:48:04 +0700

    Add HTML render for Version and Table
    Add HTML render for Version and Table.

--- lib/Pod/Goes/HTML.pm
+++ lib/Pod/Goes/HTML.pm
@@ -9,7 +9,7 @@ module Pod::Goes::HTML {
 		};
 	my @named =
 		# =begin pod
-		sub ( :$name where {$name ~~ "pod"}) { True; } => {
+		sub (:$name where {$name ~~ 'pod'}) { True; } => {
 			start => sub (:@draft) {
 				@draft.push(
 					qq[<!doctype html>{$N}<html>{$N}<head>{$N}],
@@ -19,24 +19,50 @@ module Pod::Goes::HTML {
 				True;
 			},
 			stop => sub (:@draft) {
-				@draft.push(qq[</body>{$N}/html>{$N}]);
+				@draft.push(qq[</body>{$N}</html>{$N}]);
 				True;
 			}
+		},
+		sub (:$name where {$name ~~ 'VERSION'}) { True; } => {
+			start => sub (:@draft) { push @draft, qq[<section>{$N}<h1>VERSION</h1>{$N}]; True; },
+			stop =>  sub (:@draft) { push @draft, qq[</section>{$N}]; True; },
 		};
 	my @para =
 		# Title #
-		sub (:@history where {@history && @history[*-1] ~~ Pod::Block::Named && @history[*-1].name ~~ 'TITLE'}) { True; } => {
+		sub (:@history where {@history.&under-name('TITLE')}) { True; } => {
 			in => sub (:$content, :@draft, :%storage) {
 				push @draft, qq[<h1>{$content}</h1>{$N}];
-				%storage{'TITLE'} = $content; True;
+				%storage{'TITLE'} = $content;
+				True;
 			}
 		};
+	my @table =
+		sub { True; } => {
+			start => sub (:$caption, :@headers, :@draft) {
+				push @draft, qq[<table>{$N}<thead>{$N}<tr>{$N}];
+				for @headers -> $header {
+					push @draft, qq[<th>{$header}</th>{$N}];
+				}
+				push @draft, qq[</tr>{$N}</thead>{$N}<tbody>{$N}];
+				True;
+			},
+			in => sub (:$content, :@draft) {
+				push @draft, qq[<tr>{$N}];
+				for @($content) -> $td {
+					push @draft, qq[<td>{$td}</td>{$N}];
+				}
+				push @draft, qq[</tr>{$N}];
+				True;
+			},
+			stop => sub (:@draft) {	push @draft, qq[</tbody>{$N}</table>{$N}]; True; }
+		}
 
 	sub make-nearer() is export {
 		my Nearer $nearer .= new;
 		$nearer.callbacks{Pod::Block::Comment.^name} = @comment;
 		$nearer.callbacks{Pod::Block::Named.^name} = @named;
 		$nearer.callbacks{Pod::Block::Para.^name} = @para;
+		$nearer.callbacks{Pod::Block::Table.^name} = @table;
 
 		return $nearer;
 	}
--- t/01 - html.pm
+++ t/01 - html.pm
@@ -12,3 +12,6 @@ ok $nearer, 'make caller works well';
 my $pod = get-pod slurp('../in.txt'.path);
 $nearer.approach-to($pod);
 say "draft is:\n{$nearer.get-result}\n" ~ '-' x 30;
+my $out = open 'html-out.html', :w;
+$out.say($nearer.get-result);
+$out.close();
