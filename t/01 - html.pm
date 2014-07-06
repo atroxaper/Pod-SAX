3f5358d164f42fc7f2a105fd5073007609eb26ce
Mikhail Khorkov <m.s.khorkov@gmail.com>
2014-07-06 21:30:47 +0700

    Add some HTML render tests
    Add some HTML render tests.

--- t/01 - html.pm
+++ t/01 - html.pm
@@ -5,16 +5,20 @@ use Test;
 use Pod::Goes::HTML;
 use Pod::Nearby;
 
-plan 1;
+plan 3;
+
+sub rm-n($str) {
+	return $str.subst(/\n/, '', :g);
+}
 
 sub get-test-result(Str $source --> Str) {
 	my Nearer $nearer = make-nearer;
 	my $pod = get-pod($source);
 	$nearer.approach-to($pod);
-	return $nearer.get-result.subst(/\n/, '', :g);
+	return $nearer.get-result.&rm-n;
 }
 
-{# title
+{# start and end of HTML file
 	my $pod-str = qq:to[END];
 		=begin pod
 
@@ -23,13 +27,36 @@ sub get-test-result(Str $source --> Str) {
 
 		=end pod
 		END
-	my $expect = qq:to[END];
-		<!doctype html><html><head>
-		<title>This Title</title>
-		</head><body class="pod" id="___top">
-		<h1>This Title</h1>
-		</body></html>
+	is get-test-result($pod-str),
+		q[<!doctype html><html><head>] ~
+		q[<title>This Title</title>] ~
+		q[</head><body class="pod" id="___top">] ~
+		q[<h1>This Title</h1>] ~
+		q[</body></html>], 'just pod';
+}
+
+{# title
+	my $pod-str = qq:to[END];
+		=TITLE
+		This Title
 		END
-	is get-test-result($pod-str), $expect.subst(/\n/, '', :g), 'just title';
+	is get-test-result($pod-str), q[<h1>This Title</h1>], 'just title';
+}
+
+{# heading
+	my $pod-str = qq:to[END];
+		=head1
+		Heading1
+
+		=head2
+		Heading2
+
+		=head1
+		Heading11
+		END
+	is get-test-result($pod-str),
+		q[<h1>Heading1</h1>] ~
+		q[<h2>Heading2</h2>] ~
+		q[<h1>Heading11</h1>], 'just headings';
 }
 
